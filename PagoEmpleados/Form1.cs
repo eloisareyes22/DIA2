ee95fd9f6239a1614cc899f27747c0d8ea849cbc
JADPA-07 <JADPA-07@JADPA-07>
2022-08-04 10:22:18 -0600

    Agregar archivos de proyecto.

---
+++ PagoEmpleados/Form1.cs
@@ -0,0 +1,30 @@
+namespace PagoEmpleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
